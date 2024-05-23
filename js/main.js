38ca733bdb658ca3c2005c2958c7383ee3e7b6f7
CrashStatic <thecrashstatic@gmail.com>
2024-05-23 13:59:26 +0300

    Менеяет функцию на отображение только одного объявления

--- js/main.js
+++ js/main.js
@@ -3,10 +3,4 @@ import { generateAdvertisement } from './data.js';
 
 const advertisements = generateAdvertisement();
 
-// const firstAdvertisement = advertisements[0];
-
-// console.log(firstAdvertisement);
-
-// getSimilarAdvertisemen(firstAdvertisement);
-
-getSimilarAdvertisemen(advertisements);
+getSimilarAdvertisemen(advertisements[0]);
--- js/similar-ads.js
+++ js/similar-ads.js
@@ -6,36 +6,34 @@ const advertisementTemplate = document.querySelector('#card').content.querySelec
 const getSimilarAdvertisemen = (element) => {
   const fragment = document.createDocumentFragment();
 
-  element.forEach(({ offer, author }) => {
-    const advertisementElement = advertisementTemplate.cloneNode(true);
-    advertisementElement.querySelector('.popup__title').textContent = offer.title;
-    advertisementElement.querySelector('.popup__text--address').textContent = offer.address;
-    advertisementElement.querySelector('.popup__text--price').textContent = `${offer.price} ₽/ночь`;
-    advertisementElement.querySelector('.popup__type').textContent = offer.type.name;
-    advertisementElement.querySelector('.popup__text--capacity').textContent = `${offer.rooms} ${getEnding(offer.rooms)} для ${offer.guests} гостей`;
-    advertisementElement.querySelector('.popup__text--time').textContent = `Заезд после ${offer.checkin}, выезд до ${offer.checkout}`;
+  const advertisementElement = advertisementTemplate.cloneNode(true);
+  advertisementElement.querySelector('.popup__title').textContent = element.offer.title;
+  advertisementElement.querySelector('.popup__text--address').textContent = element.offer.address;
+  advertisementElement.querySelector('.popup__text--price').textContent = `${element.offer.price} ₽/ночь`;
+  advertisementElement.querySelector('.popup__type').textContent = element.offer.type.name;
+  advertisementElement.querySelector('.popup__text--capacity').textContent = `${element.offer.rooms} ${getEnding(element.offer.rooms)} для ${element.offer.guests} гостей`;
+  advertisementElement.querySelector('.popup__text--time').textContent = `Заезд после ${element.offer.checkin}, выезд до ${element.offer.checkout}`;
 
-    advertisementElement.querySelector('.popup__features').textContent = '';
-    for (let i = 0; i < offer.features.length; i++) {
-      const popupFeaturesElement = document.createElement('li');
-      popupFeaturesElement.classList.add('popup__feature');
-      popupFeaturesElement.classList.add(`popup__feature--${offer.features[i]}`);
-      advertisementElement.querySelector('.popup__features').append(popupFeaturesElement);
-    }
+  advertisementElement.querySelector('.popup__features').textContent = '';
+  for (let i = 0; i < element.offer.features.length; i++) {
+    const popupFeaturesElement = document.createElement('li');
+    popupFeaturesElement.classList.add('popup__feature');
+    popupFeaturesElement.classList.add(`popup__feature--${element.offer.features[i]}`);
+    advertisementElement.querySelector('.popup__features').append(popupFeaturesElement);
+  }
 
-    advertisementElement.querySelector('.popup__description').textContent = offer.description;
+  advertisementElement.querySelector('.popup__description').textContent = element.offer.description;
 
-    const firstPhotoElement = advertisementElement.querySelector('.popup__photo');
-    for (let i = 0; i < offer.photos.length; i++) {
-      const popupPhotosElement = firstPhotoElement.cloneNode(true);
-      popupPhotosElement.src = offer.photos[i];
-      advertisementElement.querySelector('.popup__photos').appendChild(popupPhotosElement);
-    }
-    advertisementElement.querySelector('.popup__photos').removeChild(firstPhotoElement);
+  const firstPhotoElement = advertisementElement.querySelector('.popup__photo');
+  for (let i = 0; i < element.offer.photos.length; i++) {
+    const popupPhotosElement = firstPhotoElement.cloneNode(true);
+    popupPhotosElement.src = element.offer.photos[i];
+    advertisementElement.querySelector('.popup__photos').appendChild(popupPhotosElement);
+  }
+  advertisementElement.querySelector('.popup__photos').removeChild(firstPhotoElement);
 
-    advertisementElement.querySelector('.popup__avatar').src = author.avatar;
-    fragment.appendChild(advertisementElement);
-  });
+  advertisementElement.querySelector('.popup__avatar').src = element.author.avatar;
+  fragment.appendChild(advertisementElement);
 
   containerElement.appendChild(fragment);
 };
