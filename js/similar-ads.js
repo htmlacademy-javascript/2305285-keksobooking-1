fdd793376f6ce9ef1b5da79de436031670719ec0
CrashStatic <thecrashstatic@gmail.com>
2024-05-23 12:19:27 +0300

    Заполняет карточку информацией

--- js/similar-ads.js
+++ js/similar-ads.js
@@ -6,12 +6,33 @@ const advertisementTemplate = document.querySelector('#card').content.querySelec
 const getSimilarAdvertisemen = (element) => {
   const fragment = document.createDocumentFragment();
 
-  element.forEach(({ offer }) => {
+  element.forEach(({ offer, author }) => {
     const advertisementElement = advertisementTemplate.cloneNode(true);
     advertisementElement.querySelector('.popup__title').textContent = offer.title;
     advertisementElement.querySelector('.popup__text--address').textContent = offer.address;
     advertisementElement.querySelector('.popup__text--price').textContent = `${offer.price} ₽/ночь`;
     advertisementElement.querySelector('.popup__text--capacity').textContent = `${offer.rooms} ${getEnding(offer.rooms)} для ${offer.guests} гостей`;
+    advertisementElement.querySelector('.popup__text--time').textContent = `Заезд после ${offer.checkin}, выезд до ${offer.checkout}`;
+
+    advertisementElement.querySelector('.popup__features').textContent = '';
+    for (let i = 0; i < offer.features.length; i++) {
+      const popupFeaturesElement = document.createElement('li');
+      popupFeaturesElement.classList.add('popup__feature');
+      popupFeaturesElement.classList.add(`popup__feature--${offer.features[i]}`);
+      advertisementElement.querySelector('.popup__features').append(popupFeaturesElement);
+    }
+
+    advertisementElement.querySelector('.popup__description').textContent = offer.description;
+
+    const firstPhotoElement = advertisementElement.querySelector('.popup__photo');
+    for (let i = 0; i < offer.photos.length; i++) {
+      const popupPhotosElement = firstPhotoElement.cloneNode(true);
+      popupPhotosElement.src = offer.photos[i];
+      advertisementElement.querySelector('.popup__photos').appendChild(popupPhotosElement);
+    }
+    advertisementElement.querySelector('.popup__photos').removeChild(firstPhotoElement);
+
+    advertisementElement.querySelector('.popup__avatar').src = author.avatar;
     fragment.appendChild(advertisementElement);
   });
 
@@ -19,3 +40,10 @@ const getSimilarAdvertisemen = (element) => {
 };
 
 export { getSimilarAdvertisemen };
+
+// advertisementElement.querySelector('.popup__features').textContent = offer.features;
+
+// const popupFeaturesElement = document.createElement('li');
+// popupFeaturesElement.classList.add('popup__feature');
+// popupFeaturesElement.classList.add(`popup__feature--${offer.features[0]}`);
+// advertisementElement.querySelector('.popup__features').append(popupFeaturesElement);
